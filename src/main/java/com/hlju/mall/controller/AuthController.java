0309cfd608cdf399a692f067fbdf511165dcc732
54575 <54575@DESKTOP-CA4PGCS>
2018-11-17 19:03:04 +0800

    #test#

--- src/main/java/com/hlju/mall/controller/AuthController.java
+++ src/main/java/com/hlju/mall/controller/AuthController.java
@@ -14,7 +14,7 @@ import com.hlju.mall.service.UserService;
 @Controller
 public class AuthController {
 	
-	@Autowired
+	@Autowired 
 	UserService userService;
 
 	@RequestMapping(method = RequestMethod.POST, value = { "/register", "/register.html" })
